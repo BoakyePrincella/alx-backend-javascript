ca08ad079c4f528b7cc9f9443c55f1f5abdf3376
BoakyePrincella <princellaboakye2003@gmail.com>
2024-11-12 08:46:22 +0000

    ES6-classes

--- 0x02-ES6_classes/100-evcar.js
+++ 0x02-ES6_classes/100-evcar.js
@@ -1,9 +1,10 @@
-import Car from './10-car.js';
+import Car from './10-car';
 
 export default class EVCar extends Car {
   constructor(brand, motor, color, range) {
     super(brand, motor, color);
     this._range = range;
+  }
 
   get range() {
     return this._range;
